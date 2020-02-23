58e045e6977448c27a4fdfd67b431324ee166250
Earl Potters <earl.potters@gmail.com>
2020-02-22 22:39:43 -0700

    Added an example json example file

--- EarlPython/kismetConnector_rest.py
+++ EarlPython/kismetConnector_rest.py
@@ -4,7 +4,7 @@ import time
 conn = kismet_rest.KismetConnector(username="slyracoon23", password="12345678")
 for device in conn.device_summary():
     print(device['kismet.device.base.manuf'])
-    r = requests.get(url = "http://www.google.com")
-    print(r)
+    #r = requests.get(url = "http://www.google.com")
+    #print(r)
     time.sleep(0.5)
 
