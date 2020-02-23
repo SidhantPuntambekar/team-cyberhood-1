320f3fcade5cac6444a019d9ea06d65362f75cd5
Arjun Lakshmi Narasimhan <larjun01@cu-biot-15-10.203.184.167.int.colorado.edu>
2020-02-22 21:25:06 -0700

    Added website and app

--- gui/index.java
+++ gui/index.java
@@ -1,8 +1,13 @@
 import javax.swing.*;
+import javax.swing.GroupLayout.Alignment;
+
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.Image.*;
+
 class index {
     public static void main(String args[]) {
-
+        
         //Creating the Frame
         JFrame frame = new JFrame("Chat Frame");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,23 +24,56 @@ class index {
         m1.add(m11);
         m1.add(m22);
 
-        //Creating the panel at bottom and adding components
-        JPanel panel = new JPanel(); // the panel is not visible in output
-        JButton addbtn = new JButton("Add Device");
-        JButton scanbtn = new JButton("Scan");
-        JButton analysebtn = new JButton("Analyse");
-        // Components Added using Flow Layout
-        panel.add(addbtn);
-        panel.add(scanbtn);
-        panel.add(analysebtn);
-
         // Text Area at the Center
-        JTextArea ta = new JTextArea();
-
+        JPanel ta = new JPanel();
+        ImageIcon scanning = new ImageIcon("ajax-loader.gif");
+        JLabel scanstatus = new JLabel("Scanning... ", scanning, JLabel.CENTER);
+        ta.add(scanstatus);
+        scanstatus.setVisible(false);
+        
+        //Creating the panel at bottom and adding components
+        //JPanel btnlayer = new JPanel(); // the panel is not visible in output
+        JPanel btnlayer = new JPanel(); // the panel is not visible in output
+        
+        JLabel logo = new JLabel(new ImageIcon("logo.jpeg"));
+        logo.setSize(50,50);
+        logo.setAlignmentX(Component.LEFT_ALIGNMENT);
+        JButton addbtn = new JButton("Add Device");
+        addbtn.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        JButton scanbtn = new JButton("Scan");
+        scanbtn.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        JButton analysebtn = new JButton("Analyse");
+        analysebtn.setAlignmentX(Component.RIGHT_ALIGNMENT);
+        
+        // Components Added using Flow Layout
+        btnlayer.add(logo);
+        btnlayer.add(addbtn);
+        btnlayer.add(scanbtn);
+        btnlayer.add(analysebtn);
+        
         //Adding Components to the frame.
-        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(BorderLayout.CENTER, btnlayer);
         frame.getContentPane().add(BorderLayout.NORTH, mb);
         frame.getContentPane().add(BorderLayout.SOUTH, ta);
         frame.setVisible(true);
+        
+        scanbtn.addActionListener(new ActionListener() {
+            
+            public void actionPerformed(ActionEvent evt) {
+                scanstatus.setVisible(true);
+            }
+          
+        });
     }
+    /*
+    JFrame frame = new JFrame("Test");
+
+    ImageIcon scanning = new ImageIcon("ajax-loader.gif");
+    frame.add(new JLabel("Scanning... ", scanning, JLabel.CENTER));
+
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(400, 300);
+    frame.setVisible(true);
+    */
 }
---
+++ website/script/index.js
@@ -0,0 +1,15 @@
+var addBtn = document.querySelector("#addbtn");
+var scanBtn = document.querySelector("#scanbtn");
+var anaBtn = document.querySelector("#anabtn");
+
+addBtn.addEventListener("click", function () {
+    alert("Device add button has been clicked!");
+})
+
+scanBtn.addEventListener("click", function () {
+    alert("Scan button has been clicked!");
+})
+
+anaBtn.addEventListener("click", function () {
+    alert("Analyse button has been clicked!");
+})
