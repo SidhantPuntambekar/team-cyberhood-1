35ae6368bba49ce0beac93a7e2265711393d31cb
Kieran Zylstra <kieran.zylstra@colorado.edu>
2020-02-23 07:40:57 -0700

    adding html

--- website/script/index.js
+++ website/script/index.js
@@ -13,3 +13,42 @@ scanBtn.addEventListener("click", function () {
 anaBtn.addEventListener("click", function () {
     alert("Analyze button has been clicked!");
 })
+
+function displayTable1() {
+    var table1 = document.getElementById("table1");
+    var table2 = document.getElementById("table2");
+    //var table2 = document.getElementById("table2")
+    //var table2 = document.getElementById("table2")
+    table2.height = 0;
+    table2.style.visibility = "hidden";
+    table1.height = "480";
+    table1.style.visibility = "visible";
+    document.getElementById("table1").contentWindow.location.reload();
+   
+}
+
+function displayTable2() {
+    var table1 = document.getElementById("table1");
+    var table2 = document.getElementById("table2");
+    //var table2 = document.getElementById("table2")
+    //var table2 = document.getElementById("table2")
+    table1.height = 0;
+    table1.style.visibility = "hidden";
+    table2.height = "480";
+    table2.style.visibility = "visible";
+    document.getElementById("table2").contentWindow.location.reload();
+
+
+}
+
+function displayTable3() {
+
+}
+
+window.setInterval(function() {
+            reloadIFrame()
+        }, 3000);
+
+ function reloadIFrame() {
+            console.log('reloading..');
+        }
